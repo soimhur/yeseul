7c36de2e57deee80ff56a02e55d37994acb79885
soimhur <soimhur@gmail.com>
2018-07-30 12:07:31 +0900

    Add analytics

--- src/analytics.js
+++ src/analytics.js
@@ -10,5 +10,5 @@
   m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-118177426-1', 'auto');
+ga('create', 'UA-123010014-1', 'auto');
 ga('send', 'pageview');
